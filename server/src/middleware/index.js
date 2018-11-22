5d8f2f592114f35ceecb5e448b90acb0f2e8f57f
Iyk Azorji <iykazorji@gmail.com>
2018-11-22 01:50:36 +0100

    Ran a test on the middleware infrastructure

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -1,5 +1,10 @@
 // eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
-  // Add your custom middleware here. Remember that
-  // in Express, the order matters.
+module.exports = function(app) {
+	// Add your custom middleware here. Remember that
+	// in Express, the order matters.
+	app.get('/test', (req, res) => {
+		res.json({
+			message: 'This is just testing the middleware'
+		});
+	});
 };

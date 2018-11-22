b01dbd83064edf56f1dd6d2102c116b9542cbaff
Iyk Azorji <iykazorji@gmail.com>
2018-11-22 21:36:30 +0100

    Added oauth handler middleware

--- server/src/authentication.js
+++ server/src/authentication.js
@@ -5,35 +5,62 @@ const oauth2 = require('@feathersjs/authentication-oauth2');
 const GoogleStrategy = require('passport-google-oauth20');
 const FacebookStrategy = require('passport-facebook');
 
-module.exports = function (app) {
-  const config = app.get('authentication');
+// Get Oauth Handler Middleware
+// This is very Key as we want a custom redirect for the token...
+const handlerMiddleWare = require('./middleware/oauth-handler');
 
-  // Set up authentication with the secret
-  app.configure(authentication(config));
-  app.configure(jwt());
-  app.configure(local());
+module.exports = function(app) {
+	const config = app.get('authentication');
 
-  app.configure(oauth2(Object.assign({
-    name: 'google',
-    Strategy: GoogleStrategy
-  }, config.google)));
+	// Set up authentication with the secret
+	app.configure(authentication(config));
+	app.configure(jwt());
+	app.configure(local());
 
-  app.configure(oauth2(Object.assign({
-    name: 'facebook',
-    Strategy: FacebookStrategy
-  }, config.facebook)));
+	// App object to handler
+	const handler = handlerMiddleWare(app);
 
-  // The `authentication` service is used to create a JWT.
-  // The before `create` hook registers strategies that can be used
-  // to create a new valid JWT (e.g. local or oauth2)
-  app.service('authentication').hooks({
-    before: {
-      create: [
-        authentication.hooks.authenticate(config.strategies)
-      ],
-      remove: [
-        authentication.hooks.authenticate('jwt')
-      ]
-    }
-  });
+	app.configure(
+		oauth2(
+			Object.assign(
+				{
+					name: 'google',
+					Strategy: GoogleStrategy,
+					handler: handler(config.google.successRedirect)
+				},
+				config.google
+			)
+		)
+	);
+
+	app.configure(
+		oauth2(
+			Object.assign(
+				{
+					name: 'facebook',
+					Strategy: FacebookStrategy
+				},
+				config.facebook
+			)
+		)
+	);
+
+	// The `authentication` service is used to create a JWT.
+	// The before `create` hook registers strategies that can be used
+	// to create a new valid JWT (e.g. local or oauth2)
+	app.service('authentication').hooks({
+		before: {
+			create: [ authentication.hooks.authenticate(config.strategies) ],
+			remove: [ authentication.hooks.authenticate('jwt') ]
+		},
+		after: {
+			create: [
+				(context) => {
+					// Get the user ID:
+					const user_id = context.params.user._id;
+					context.result = { user_id, ...context.result };
+				}
+			]
+		}
+	});
 };

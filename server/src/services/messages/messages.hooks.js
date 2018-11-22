8cfec8a230e46569c28d1f558389d7fbe2263fca
Iyk Azorji <iykazorji@gmail.com>
2018-11-23 00:23:10 +0100

    Added in Gravatar hook

--- server/src/services/messages/messages.hooks.js
+++ server/src/services/messages/messages.hooks.js
@@ -2,12 +2,14 @@ const { authenticate } = require('@feathersjs/authentication').hooks;
 
 const processMessages = require('../../hooks/process-messages');
 
+const gravatar = require('../../hooks/gravatar');
+
 module.exports = {
 	before: {
 		all: [ authenticate('jwt') ],
 		find: [],
 		get: [],
-		create: [processMessages()],
+		create: [processMessages(), gravatar()],
 		update: [],
 		patch: [],
 		remove: []

92678c0ce3d797e19dd75ea6e2cc701aa2f57bd7
Iyk Azorji <iykazorji@gmail.com>
2018-11-23 00:29:46 +0100

    completed gravatar hook

--- server/src/hooks/gravatar.js
+++ server/src/hooks/gravatar.js
@@ -2,8 +2,23 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
 // eslint-disable-next-line no-unused-vars
-module.exports = function (options = {}) {
-  return async context => {
-    return context;
-  };
+
+const crypto = require('crypto');
+
+// The gravatar image service...
+const gravatarUrl = 'https://s.gravatar.com/avatar';
+const query = 's=60';
+
+module.exports = function(options = {}) {
+	return async (context) => {
+		const { email } = context.data;
+
+		//  Create Hash for Gravatar to use to create the images...
+		const hash = crypto.createHash('md5').update(email).digest('hex');
+
+		// Add avatar information to Data being sent for processing...
+		context.data.avatar = `${gravatarUrl}/${hash}?${query}`;
+
+		return context;
+	};
 };

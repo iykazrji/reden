1cc22f4ad9e8643d7831bc3405758d8aebbe0393
Iyk Azorji <iykazorji@gmail.com>
2018-11-22 23:49:05 +0100

    Added userId parameter to responseURL

--- server/src/app.js
+++ server/src/app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
-app.use('/', express.static(app.get('public')));
+app.use('/', express.static(app.get('public'), { index: [ 'index.html' ], extensions: [ 'html' ] }));
 
 // Set JSON Spaces
 app.set('json spaces', 2);
--- server/src/middleware/oauth-handler.js
+++ server/src/middleware/oauth-handler.js
@@ -1,4 +1,11 @@
 module.exports = (app) => {
+	// This middleware handles the OAuth redirect in a more stateless way
+	// (I.E Withoud the use of cookies as is the default method Feathers would rather deal
+	//  With authentication.)
+	//  It adds the access token as part of a provieded success redirect URL.
+	//  The script in the Redirect url intercepts the token value, stores it in the browser's
+	//  Localstorage which is then now accessible for further authorization purposes.
+
 	return function(url) {
 		const config = app.get('authentication');
 		const options = {
@@ -8,10 +15,12 @@ module.exports = (app) => {
 
 		return function(req, res, next) {
 			if (req.feathers && req.feathers.payload) {
+				const userId = req.feathers.payload.userId;
+				console.log(userId);
 				app.passport
 					.createJWT(req.feathers.payload, options)
 					.then((token) => {
-						res.redirect(`${url}?token=${token}`);
+						res.redirect(`${url}?token=${token}&userId=${userId}`);
 					})
 					.catch((error) => {
 						next(error);

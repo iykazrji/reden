30facefad95a3ad61353b5069622b97f1653b620
Iyk Azorji <iykazorji@gmail.com>
2018-12-24 15:25:04 +0100

    Completed the offline tag hook

--- server/src/hooks/add-offline-tag-to-user.js
+++ server/src/hooks/add-offline-tag-to-user.js
@@ -3,19 +3,23 @@
 
 // eslint-disable-next-line no-unused-vars
 const logger = require('../logger');
+const jwt = require('jsonwebtoken');
 
 module.exports = function(options = {}) {
 	return async (context) => {
 		try {
 			const { params, app, id } = context;
+			const jwtSecret = app.get('authentication').secret;
 
-			console.log('Params object:', id);
+			// Context.id contains the JWT sent from the client...
+			// Decode the JWT sent from the client and use it to idenfify the
+			// User...
+			const decoded = jwt.verify(id, jwtSecret);
+			const user_id = decoded.userId;
 
-			// get the user_id
-			const user_id = params.user._id;
+			// Patch the user data with the offline tag...
+			app.service('users').patch(user_id, { online: false }, params);
 
-			const user = await app.service('users').patch(user_id, { online: false }, params);
-			logger.log('User object', user);
 			return context;
 		} catch (err) {
 			logger.error('An error occured at the authentication after hook:', err);

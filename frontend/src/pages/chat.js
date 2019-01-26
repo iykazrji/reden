94141ff3baeef33925918a3a37588538e23ad0f1
Iyk Azorji <iykazorji@gmail.com>
2019-01-26 17:39:52 +0100

    Project Reset

--- frontend/src/pages/chat.js
+++ frontend/src/pages/chat.js
@@ -1,9 +1,26 @@
 import React, { Fragment } from 'react';
 import Styled from 'styled-components';
 import { observer, inject } from 'mobx-react';
+import { Flex, Box } from 'rebass';
+
+const ChatPageMainWrapperFlex = Styled(Flex)`
+    width: 100%;
+    height: 100vh;
+`;
+const ChatPageLeftColumnBox = Styled(Box)`
+    border-right: 1px solid rgba(170, 63, 60, 0.5);
+    
+`;
 
 const ChatPage = (props) => {
-	return <Fragment>ChatPage</Fragment>;
+	return (
+		<Fragment className="chat-page">
+			<ChatPageMainWrapperFlex className="chat-page-main-wrapper">
+				<ChatPageLeftColumnBox width={[ 0.3 ]}>Stuff</ChatPageLeftColumnBox>
+				<Box width={[ 0.7 ]}>Stuff</Box>
+			</ChatPageMainWrapperFlex>
+		</Fragment>
+	);
 };
 
 export default ChatPage;
--- server/src/app.js
+++ server/src/app.js
@@ -31,6 +31,7 @@ app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+
 // Host the public folder
 app.use('/', express.static(app.get('public'), { index: [ 'index.html' ], extensions: [ 'html' ] }));
 
